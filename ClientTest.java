2f8d10eb367c8dafc0214e5c556d9ef920e8200a
Таисия Ткаченко <85654567+Zetrixis@users.noreply.github.com>
2021-09-26 00:35:14 +0300

    Создала новый файл ClientTest.java

---
+++ ClientTest.java
@@ -0,0 +1,11 @@
+package com.person;
+
+public class ClientTest {
+    public void clientTest(){
+        PersonClient client = new PersonClient();
+        System.out.println("Choose a service: ");
+        System.out.println(client.service());
+        System.out.println("The cost of the service: " + client.price() + "$");
+        System.out.println("Test completed");
+    }
+}
