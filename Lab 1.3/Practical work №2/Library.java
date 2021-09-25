65e44293adbd6476d9b41997b80446510ed4d34c
Таисия Ткаченко <85654567+Zetrixis@users.noreply.github.com>
2021-09-26 02:22:20 +0300

    Создала новый файл Library.java
    Создание класса драйвера под названием библиотека, основной метод конкретизирует и обновляет несколько объектов книг.

---
+++ Lab 1.3/Practical work №2/Library.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+
+ 
+public class Library
+{
+  
+public static void main (String[]args)
+  {
+    
+Book d1 = new Book ("The Mortal Instruments", 576);
+    
+Book d2 = new Book ("Mara and Morok", 416);
+    
+Book d3 = new Book ("The Master and Margarita", 512);
+    
+Book d4 = new Book ("War and peace", 1274);
+    
+Book d5 = new Book ("Red, White & Royal Blue", 480);
+    
+System.out.println (d1, d2, d3, d4, d5);
+
+  } 
+}
