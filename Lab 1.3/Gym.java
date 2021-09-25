930cea76d82428c2cfa8426ae28f772cb4582850
Таисия Ткаченко <85654567+Zetrixis@users.noreply.github.com>
2021-09-26 01:57:33 +0300

    Создала новый файл Gym.java
    Создание класса драйвера под названием физкультурный зал, основной метод конкретизирует и обновляет несколько объектов мяча.

---
+++ Lab 1.3/Gym.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+ 
+public class Nursery
+{
+  
+public static void main (String[]args)
+  {
+    
+Ball d1 = new Ball ("Tennis ball ", 2);
+    
+Ball d2 = new Ball ("Volleyball", 7);
+    
+Ball d3 = new Ball ("Basketball");
+    
+Ball d4 = new Ball ("Golf ball", 8);
+    
+Ball d5 = new Ball ("Football", 1);
+    
+d3.setNumber (1);
+    
+System.out.println (d1, d2, d3, d4, d5);
+
+  } 
+}
