53d46aff70d92797362a33a4b16235e34890dfbc
Таисия Ткаченко <85654567+Zetrixis@users.noreply.github.com>
2021-12-10 16:41:36 +0300

    Create Controller.java

---
+++ Lab 3.1/Controller.java
@@ -0,0 +1,23 @@
+package Tkachenko.Lab22;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class Controller {
+    Database db = new Database();
+
+    public Controller() {
+        try {
+            db.connect();
+            db.getFromDatabase();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addEmployee(Employee employee) {
+        db.addEmployeeToDatabase(employee);
+    }
+
+    public List<Employee> getEmployee() { return db.getEmployees(); }
+}
