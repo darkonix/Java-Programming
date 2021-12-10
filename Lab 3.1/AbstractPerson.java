29ca38f724b6c988b57525ea0613e6121a9639a1
Таисия Ткаченко <85654567+Zetrixis@users.noreply.github.com>
2021-12-10 16:40:03 +0300

    Create AbstractPerson.java

---
+++ Lab 3.1/AbstractPerson.java
@@ -0,0 +1,15 @@
+package Tkachenko.Lab31;
+
+public abstract class AbstractPerson implements HumanInterface{
+    private String name;
+
+    AbstractPerson(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String think();
+}
